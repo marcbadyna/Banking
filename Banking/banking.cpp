3641072b6ca40d4aba145370ed58601fcfd6c757
marcbadyna <150734285+marcbadyna@users.noreply.github.com>
2025-04-23 08:27:54 -0400

    Logic for displayAccount function.

--- Banking/banking.cpp
+++ Banking/banking.cpp
@@ -5,5 +5,14 @@
 //  Created by Marc Badyna on 4/23/25.
 //
 #include "banking.h"
+#include <iostream>
 
 Account::Account(int accountNumber, std::string name, double balance) : accountNumber(accountNumber), name(name), balance(balance) {}
+
+Bank::Bank() : nextAccountNumber(1) {}
+
+void Account::displayAccount() {
+    std::cout << "Account Number: " << accountNumber << std::endl;
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Balance: " << balance << std::endl;
+}
